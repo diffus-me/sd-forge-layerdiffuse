be4cccea1611a4e53d3dec4ce0ebe13016350a45
linji.xue <linji.xue@graviti.com>
2025-01-07 18:11:33 +0800

    feat: add elem_id to "ForgeCanvas" in "forge_layerdiffusion.py"

--- scripts/forge_layerdiffusion.py
+++ scripts/forge_layerdiffusion.py
@@ -74,13 +74,13 @@ class LayerDiffusionForForge(scripts.Script):
             with gr.Row():
                 with gr.Column(visible=False) as fg_col:
                     gr.Markdown('Foreground')
-                    fg_image = ForgeCanvas(numpy=True, no_scribbles=True, height=300).background
+                    fg_image = ForgeCanvas(numpy=True, no_scribbles=True, height=300, elem_id=f"{tabname}-layerdiffusion-foreground").background
                 with gr.Column(visible=False) as bg_col:
                     gr.Markdown('Background')
-                    bg_image = ForgeCanvas(numpy=True, no_scribbles=True, height=300).background
+                    bg_image = ForgeCanvas(numpy=True, no_scribbles=True, height=300, elem_id=f"{tabname}-layerdiffusion-background").background
                 with gr.Column(visible=False) as blend_col:
                     gr.Markdown('Blending')
-                    blend_image = ForgeCanvas(numpy=True, no_scribbles=True, height=300).background
+                    blend_image = ForgeCanvas(numpy=True, no_scribbles=True, height=300, elem_id=f"{tabname}-layerdiffusion-blending").background
 
             gr.HTML('</br>')  # some strange gradio problems
 
